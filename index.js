8391bb19a523aca68c2b0f4e43743d33db83335f
joshhartigan <joshhartigan99@gmail.com>
2014-10-31 15:16:21 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+var cheerio = require('cheerio');
+var req = require('request');
+var open_ = require('open');
+
+var titleString = [
+'  _____              ___',
+' / ___/__  ___  ____/ (_)',
+'/ (_ / _ \\/ _ \\/ __/ / /',
+'\\___/\\___/\\___/\\__/_/_/'].join('\n');
+
+var query = process.argv[2];
+var linkChoice = process.argv[3];
+
+var errorEmoji = '❗';
+if ( !query ) {
+  console.log( errorEmoji + '  problem: you didn\'t search for anything.' );
+  process.exit(1);
+}
+
+searchFor(query);
+
+function searchFor(query) {
+
+  req( 'https://google.com/search?q=' + query, function(err, response, data) {
+
+    if ( err ) {
+      console.log( errorEmoji + err );
+    }
+
+    if ( response.statusCode === 404 ) {
+      console.log( errorEmoji + '  problem: incorrect query.' );
+      process.exit(1);
+    }
+
+    if ( response.statusCode === 200 ) {
+
+      $ = cheerio.load(data);
+
+      if ( !linkChoice) {
+        console.log(titleString);
+
+        $('h3.r').each( function(i, elem) {
+          process.stdout.write( '[' + i + '] ' );
+          console.log( $(this).text() );
+        });
+      } else {
+        var HRef = $('h3.r a')[ linkChoice ].attribs.href;
+        open_( HRef.slice(
+          HRef.indexOf('=') + 1,
+          HRef.indexOf('&')
+        ));
+      }
+
+    }
+
+  });
+
+}
+
